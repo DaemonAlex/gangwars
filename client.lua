178f518ee81c5181dbbb864cb877283dba1a0ecd
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2025-02-26 00:47:57 -0600

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,62 +1,27 @@
-RegisterNetEvent("gangFightNotification")
-AddEventHandler("gangFightNotification", function(location)
-  SetNotificationTextEntry("STRING")
-  AddTextComponentString("Gang fight detected nearby!")
-  DrawNotification(false, false)
+local Ox = exports.ox_lib
+
+RegisterNetEvent('gangWars:notifyGangActivity')
+AddEventHandler('gangWars:notifyGangActivity', function(message)
+    Ox.ShowNotification({
+        title = 'Gang Activity',
+        description = message,
+        type = 'error',
+        duration = 5000
+    })
 end)
 
-RegisterNetEvent("targetedNotification")
-AddEventHandler("targetedNotification", function(gangName)
-  SetNotificationTextEntry("STRING")
-  AddTextComponentString("You have been targeted by the ".. gangName.. " gang!")
-  DrawNotification(false, false)
+RegisterNetEvent('gangWars:gangFightStarted')
+AddEventHandler('gangWars:gangFightStarted', function(coords)
+    PlaySoundFromCoord(-1, "BANG", coords.x, coords.y, coords.z, "DLC_IE_Explosive_Ammo_Sounds", true, 120, true)
+    StartParticleFxLoopedAtCoord("scr_rcbarry2", coords.x, coords.y, coords.z, 0.0, 0.0, 0.0, 1.0, false, false, false, false)
 end)
 
-local gangMembers = {}
-RegisterNetEvent("gangMemberSpawned")
-AddEventHandler("gangMemberSpawned", function(gangMember)
-  table.insert(gangMembers, gangMember)
-end)
-
-Citizen.CreateThread(function()
-  while true do
-    Citizen.Wait(1000)
-   
-    for _, gangMember in ipairs(gangMembers) do
-      local ped = GetPedFromHandle(gangMember.pedHandle)
-      if DoesEntityExist(ped) then
-      
-        DrawMarker(1, gangMember.location.x, gangMember.location.y, gangMember.location.z, 0, 255, 0, 255, 1.0, 1.0, 1.0, 1.0, 1.0, 255, 255, 255, 255)
-      end
-    end
-  end
-end)
-
-RegisterNetEvent("gangMemberDeleted")
-AddEventHandler("gangMemberDeleted", function(gangMember)
-  for i, member in ipairs(gangMembers) do
-    if member.pedHandle == gangMember.pedHandle then
-      table.remove(gangMembers, i)
-    end
-  end
-end)
-
-
-Citizen.CreateThread(function()
-  while true do
-    Citizen.Wait(1000)
-    for _, gangMember in ipairs(gangMembers) do
-      local ped = GetPedFromHandle(gangMember.pedHandle)
-      if DoesEntityExist(ped) then
-        local distance = GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), gangMember.location.x, gangMember.location.y, gangMember.location.z)
-        if distance < 5.0 then
-      
-          SetNotificationTextEntry("STRING")
-          AddTextComponentString("Gang Member: ".. gangMember.name)
-          AddTextComponentString("Gang: ".. gangMember.gangName)
-          DrawNotification(false, false)
-        end
-      end
-    end
-  end
+RegisterNetEvent('gangWars:playerEnteredTerritory')
+AddEventHandler('gangWars:playerEnteredTerritory', function(territory)
+    Ox.ShowNotification({
+        title = 'Territory Alert',
+        description = 'You have entered ' .. territory .. '. Watch your back!',
+        type = 'warning',
+        duration = 7500
+    })
 end)
