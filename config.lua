629802103c00c72dbd7476728f717c3469fe7f36
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2025-03-02 12:53:11 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,27 +3,27 @@ Config = {}
 -- Gang Data (Territories, NPCs, and Vehicles)
 Config.Gangs = {
     ['Ballas'] = {
-        territory = {x = 114.3, y = -1961.1, z = 21.3},
+        territory = vector3(114.3, -1961.1, 21.3),
         models = {'g_m_y_ballaorig_01', 'g_m_y_ballasout_01', 'g_m_y_ballaeast_01', 'csb_ballasog'},
         vehicles = {'buccaneer', 'peyote', 'voodoo'}
     },
     ['Vagos'] = {
-        territory = {x = 325.2, y = -2050.4, z = 20.9},
+        territory = vector3(325.2, -2050.4, 20.9),
         models = {'g_m_y_mexgoon_01', 'g_m_y_mexgoon_02', 'g_m_y_mexgoon_03', 'csb_ortega'},
         vehicles = {'tornado3', 'chino', 'buccaneer2'}
     },
     ['Families'] = {
-        territory = {x = -154.6, y = -1608.4, z = 34.8},
+        territory = vector3(-154.6, -1608.4, 34.8),
         models = {'g_f_y_families_01', 'g_m_y_famdnf_01', 'g_m_y_famfor_01', 'csb_ramp_gang'},
         vehicles = {'greenwood', 'manana', 'tornado'}
     },
     ['Triads'] = {
-        territory = {x = -820.4, y = -700.3, z = 27.9},
+        territory = vector3(-820.4, -700.3, 27.9),
         models = {'g_m_m_chigoon_01', 'g_m_m_chigoon_02', 'g_m_y_korean_01', 'g_m_y_korean_02'},
         vehicles = {'tailgater', 'sultan', 'schafter2'}
     },
     ['Madrazo'] = {
-        territory = {x = 1391.5, y = 1152.2, z = 114.3},
+        territory = vector3(1391.5, 1152.2, 114.3),
         models = {'g_m_m_armboss_01', 'g_m_m_armlieut_01', 'g_m_m_armgoon_01', 'csb_mweather'},
         vehicles = {'xls', 'granger', 'mesa'}
     }
@@ -34,3 +34,10 @@ Config.NotificationDistance = 500.0 -- Players within this radius will receive g
 
 -- Jobs that should receive police alerts when gang fights occur
 Config.PoliceJobs = {'police', 'sheriff', 'statepolice'}
+
+-- 🚀 **Optional: Configure Gang Spawn Behavior**
+Config.GangSpawnSettings = {
+    minPeds = 2,   -- Minimum number of NPCs spawned per gang
+    maxPeds = 5,   -- Maximum number of NPCs spawned per gang
+    armed = true   -- Should NPCs spawn with weapons?
+}
