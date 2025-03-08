b9ba13b29dd7dead1ee4af96164dcfabcaca8c25
DaemonScripts <51380857+DaemonAlex@users.noreply.github.com>
2025-03-08 01:03:13 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,8 +7,11 @@ author 'Deamonalex'
 description 'Dynamic Gang Wars Script with Enhanced Interactions using ox_lib'
 version '1.0.0'
 
-shared_script '@ox_lib/init.lua'
-shared_script 'config.lua'  
+shared_scripts {
+    '@ox_lib/init.lua',  -- Ensure ox_lib loads first
+    'config.lua'
+}
+
 client_script 'client.lua'
 server_script 'server.lua'
 
