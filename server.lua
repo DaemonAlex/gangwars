47910c14760c8e88f2d4d5087dbbcd924e682375
DaemonScripts <51380857+DaemonAlex@users.noreply.github.com>
2025-03-08 01:14:35 -0600

    Update server.lua

--- server.lua
+++ server.lua
@@ -4,6 +4,11 @@ print("^2DEBUG:^0 Config.Gangs:", Config.Gangs)
 local lib = exports.ox_lib
 local QBCore = exports['qb-core']:GetCoreObject()
 
+Config = Config or { Gangs = {} }
+
+local lastWarTime = {}
+local warCooldown = 600000  -- 10 minutes cooldown
+
 RegisterNetEvent('gangWars:triggerGangWar')
 AddEventHandler('gangWars:triggerGangWar', function(gangName)
     if not gangName or not Config.Gangs[gangName] then
@@ -19,17 +24,22 @@ end)
 
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(300000)  -- Check every 5 minutes
+        Citizen.Wait(300000)  -- Every 5 minutes
 
         for gangName, gangData in pairs(Config.Gangs) do
             for rivalGang, rivalData in pairs(Config.Gangs) do
                 if gangName ~= rivalGang then
-                    local distance = #(vector3(gangData.territory[1].x, gangData.territory[1].y, gangData.territory[1].z) - 
-                                       vector3(rivalData.territory[1].x, rivalData.territory[1].y, rivalData.territory[1].z))
+                    local currentTime = GetGameTimer()
+                    if not lastWarTime[gangName] or currentTime - lastWarTime[gangName] > warCooldown then
+                        local distance = #(vector3(gangData.territory[1].x, gangData.territory[1].y, gangData.territory[1].z) - 
+                                           vector3(rivalData.territory[1].x, rivalData.territory[1].y, rivalData.territory[1].z))
 
-                    if distance < 100 then
-                        TriggerEvent('gangWars:triggerGangWar', gangName)
-                        TriggerEvent('gangWars:triggerGangWar', rivalGang)
+                        if distance < 100 then
+                            TriggerEvent('gangWars:triggerGangWar', gangName)
+                            TriggerEvent('gangWars:triggerGangWar', rivalGang)
+                            lastWarTime[gangName] = currentTime
+                            lastWarTime[rivalGang] = currentTime
+                        end
                     end
                 end
             end
@@ -41,15 +51,20 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(14400000)  -- Every 4 hours
 
-        local gangList = {"Ballas", "Vagos", "Families"}
-        local randomGang1 = gangList[math.random(#gangList)]
-        local randomGang2 = gangList[math.random(#gangList)]
-        
-        if randomGang1 ~= randomGang2 then
-            print("^3INFO:^0 A major gang war has started between " .. randomGang1 .. " and " .. randomGang2 .. "!")
-            TriggerEvent('gangWars:triggerGangWar', randomGang1)
-            TriggerEvent('gangWars:triggerGangWar', randomGang2)
+        local gangList = { "Ballas", "Vagos", "Families" }
+        math.randomseed(GetGameTimer())  
+        local shuffledGangs = {}
+
+        while #gangList > 0 do
+            local index = math.random(#gangList)
+            table.insert(shuffledGangs, gangList[index])
+            table.remove(gangList, index)
         end
+
+        local gang1, gang2 = shuffledGangs[1], shuffledGangs[2]
+        print("^3INFO:^0 A major gang war has started between " .. gang1 .. " and " .. gang2 .. "!")
+        TriggerEvent('gangWars:triggerGangWar', gang1)
+        TriggerEvent('gangWars:triggerGangWar', gang2)
     end
 end)
 
