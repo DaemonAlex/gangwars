99fa440ee7cb79b6f169142f7b5700fa9b6f25c4
DaemonScripts <51380857+DaemonAlex@users.noreply.github.com>
2025-03-08 01:03:48 -0600

    Update server.lua

--- server.lua
+++ server.lua
@@ -4,49 +4,22 @@ print("^2DEBUG:^0 Config.Gangs:", Config.Gangs)
 local lib = exports.ox_lib
 local QBCore = exports['qb-core']:GetCoreObject()
 
-Config.Gangs = {
-    ['Ballas'] = {
-        territory = {
-            {x = 114.3, y = -1961.1, z = 21.3},
-            {x = 118.3, y = -1959.1, z = 21.3}
-        },
-        models = {'g_m_y_ballaorig_01', 'g_m_y_ballasout_01', 'g_m_y_ballaeast_01', 'csb_ballasog'},
-        vehicles = {'buccaneer', 'peyote', 'voodoo'}
-    },
-    ['Vagos'] = {
-        territory = {
-            {x = 325.2, y = -2050.4, z = 20.9},
-            {x = 330.2, y = -2048.4, z = 20.9}
-        },
-        models = {'g_m_y_mexgoon_01', 'g_m_y_mexgoon_02', 'g_m_y_mexgoon_03', 'csb_ortega'},
-        vehicles = {'tornado3', 'chino', 'buccaneer2'}
-    },
-    ['Families'] = {
-        territory = {
-            {x = -154.6, y = -1608.4, z = 34.8},
-            {x = -152.0, y = -1610.4, z = 34.8}
-        },
-        models = {'g_f_y_families_01', 'g_m_y_famdnf_01', 'g_m_y_famfor_01', 'csb_ramp_gang'},
-        vehicles = {'greenwood', 'manana', 'tornado'}
-    }
-}
-
 RegisterNetEvent('gangWars:triggerGangWar')
 AddEventHandler('gangWars:triggerGangWar', function(gangName)
-    if Config.Gangs[gangName] then
-        print("^3INFO:^0 Gang War Started: " .. gangName)
-        
-        TriggerClientEvent('gangwars:spawnGangMembers', -1, Config.Gangs[gangName])
-
-        TriggerClientEvent('gangWars:gangFightStarted', -1, Config.Gangs[gangName].territory)
-    else
-        print("^1ERROR:^0 Gang not found: " .. gangName)
+    if not gangName or not Config.Gangs[gangName] then
+        print("^1ERROR:^0 Invalid gang war trigger. Gang not found: " .. tostring(gangName))
+        return
     end
+
+    print("^3INFO:^0 Gang War Started: " .. gangName)
+    
+    TriggerClientEvent('gangwars:spawnGangMembers', -1, Config.Gangs[gangName])
+    TriggerClientEvent('gangWars:gangFightStarted', -1, Config.Gangs[gangName].territory)
 end)
 
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(60000)  -- Check every 60 seconds
+        Citizen.Wait(300000)  -- Check every 5 minutes
 
         for gangName, gangData in pairs(Config.Gangs) do
             for rivalGang, rivalData in pairs(Config.Gangs) do
@@ -54,7 +27,7 @@ Citizen.CreateThread(function()
                     local distance = #(vector3(gangData.territory[1].x, gangData.territory[1].y, gangData.territory[1].z) - 
                                        vector3(rivalData.territory[1].x, rivalData.territory[1].y, rivalData.territory[1].z))
 
-                    if distance < 100 then  -- If territories are close, start a war
+                    if distance < 100 then
                         TriggerEvent('gangWars:triggerGangWar', gangName)
                         TriggerEvent('gangWars:triggerGangWar', rivalGang)
                     end
@@ -82,8 +55,11 @@ end)
 
 RegisterNetEvent('gangWars:playerAttackedGang')
 AddEventHandler('gangWars:playerAttackedGang', function(gangName)
-    if Config.Gangs[gangName] then
-        print("^1ALERT:^0 Player attack detected on " .. gangName .. "! Retaliation initiated.")
-        TriggerEvent('gangWars:triggerGangWar', gangName)
+    if not gangName or not Config.Gangs[gangName] then
+        print("^1ERROR:^0 Invalid gang retaliation. Gang not found: " .. tostring(gangName))
+        return
     end
+
+    print("^1ALERT:^0 Player attack detected on " .. gangName .. "! Retaliation initiated.")
+    TriggerEvent('gangWars:triggerGangWar', gangName)
 end)
